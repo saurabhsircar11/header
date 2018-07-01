c46177ac3d01519ffa4e0ff251a2b4f16fa02ed9
sever <saurabh.sircar@theleanapps.com>
2018-07-01 15:13:38 +0530

    fixed issues

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <header>
+                <h1>Logo</h1>
+                <nav>
+                    <ul>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </header>
+    )
+        ;
+    }
 }
 
 export default App;

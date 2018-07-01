d7c07241266f8b27584237d8ff08aa880ae01e54
sever <saurabh.sircar@theleanapps.com>
2018-07-01 14:29:48 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+class App extends Component {
+    render() {
+        return (
+            <header>
+                <h1>Logo</h1>
+                <nav>
+                    <ul>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </header>
+        )
+            ;
+    }
+}
+
+export default App;
